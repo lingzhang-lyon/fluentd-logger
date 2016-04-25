2327e15772e2a2113c219106c26e0c3e4998f0b7
Ling Zhang <zhangling.ice@gmail.com>
2016-04-25 10:08:55 -0700

    test2

--- test.rb
+++ test.rb
@@ -1,4 +1,5 @@
 require 'fluent-logger'
 Fluent::Logger::FluentLogger.open(nil, :host=>'localhost', :port=>24224)
 Fluent::Logger.post("Qualia.cli.follow", {"from"=>"userA", "to"=>"userB"}
-# test)
+# test
+# test
